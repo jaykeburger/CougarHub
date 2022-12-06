619b5bd6cc7d052b86d7e4b97b5aa4f20b439dda
Jake <jaykegamer12@gmail.com>
2022-12-05 18:59:43 -0600

    added LoginSequence and clicklistener

--- app/src/main/java/android/example/cougarhub/MainActivity.java
+++ app/src/main/java/android/example/cougarhub/MainActivity.java
@@ -3,12 +3,24 @@ package android.example.cougarhub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.loginpage);
+
+        Button button = (Button) findViewById(R.id.loginbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                LoginSequence();
+            }
+        });
+    }
+    public void LoginSequence() {
+        setContentView(R.layout.jakepage);
     }
 }
